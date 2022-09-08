5c82d6943ab254779e37a836fdc36c7fa568c8d4
George Zaki <george.zaki@nih.gov>
2022-09-08 15:09:13 -0400

    Commented example code

--- R/normalization.R
+++ R/normalization.R
@@ -2,47 +2,47 @@
 #http://www.bioconductor.org/packages/release/workflows/vignettes/GeoMxWorkflows/inst/doc/GeomxTools_RNA-NGS_Analysis.html#5_Normalization
 
 
-library(reshape2)  # for melt
-library(cowplot)   # for plot_grid
-
-# Graph Q3 value vs negGeoMean of Negatives
-ann_of_interest <- "region"
-Stat_data <- 
-  data.frame(row.names = colnames(exprs(target_demoData)),
-             Segment = colnames(exprs(target_demoData)),
-             Annotation = pData(target_demoData)[, ann_of_interest],
-             Q3 = unlist(apply(exprs(target_demoData), 2,
-                               quantile, 0.75, na.rm = TRUE)),
-             NegProbe = exprs(target_demoData)[neg_probes, ])
-Stat_data_m <- melt(Stat_data, measure.vars = c("Q3", "NegProbe"),
-                    variable.name = "Statistic", value.name = "Value")
-
-plt1 <- ggplot(Stat_data_m,
-               aes(x = Value, fill = Statistic)) +
-  geom_histogram(bins = 40) + theme_bw() +
-  scale_x_continuous(trans = "log2") +
-  facet_wrap(~Annotation, nrow = 1) + 
-  scale_fill_brewer(palette = 3, type = "qual") +
-  labs(x = "Counts", y = "Segments, #")
-
-plt2 <- ggplot(Stat_data,
-               aes(x = NegProbe, y = Q3, color = Annotation)) +
-  geom_abline(intercept = 0, slope = 1, lty = "dashed", color = "darkgray") +
-  geom_point() + guides(color = "none") + theme_bw() +
-  scale_x_continuous(trans = "log2") + 
-  scale_y_continuous(trans = "log2") +
-  theme(aspect.ratio = 1) +
-  labs(x = "Negative Probe GeoMean, Counts", y = "Q3 Value, Counts")
-
-plt3 <- ggplot(Stat_data,
-               aes(x = NegProbe, y = Q3 / NegProbe, color = Annotation)) +
-  geom_hline(yintercept = 1, lty = "dashed", color = "darkgray") +
-  geom_point() + theme_bw() +
-  scale_x_continuous(trans = "log2") + 
-  scale_y_continuous(trans = "log2") +
-  theme(aspect.ratio = 1) +
-  labs(x = "Negative Probe GeoMean, Counts", y = "Q3/NegProbe Value, Counts")
-
-btm_row <- plot_grid(plt2, plt3, nrow = 1, labels = c("B", ""),
-                     rel_widths = c(0.43,0.57))
-plot_grid(plt1, btm_row, ncol = 1, labels = c("A", ""))
+#library(reshape2)  # for melt
+#library(cowplot)   # for plot_grid
+#
+## Graph Q3 value vs negGeoMean of Negatives
+#ann_of_interest <- "region"
+#Stat_data <- 
+#  data.frame(row.names = colnames(exprs(target_demoData)),
+#             Segment = colnames(exprs(target_demoData)),
+#             Annotation = pData(target_demoData)[, ann_of_interest],
+#             Q3 = unlist(apply(exprs(target_demoData), 2,
+#                               quantile, 0.75, na.rm = TRUE)),
+#             NegProbe = exprs(target_demoData)[neg_probes, ])
+#Stat_data_m <- melt(Stat_data, measure.vars = c("Q3", "NegProbe"),
+#                    variable.name = "Statistic", value.name = "Value")
+#
+#plt1 <- ggplot(Stat_data_m,
+#               aes(x = Value, fill = Statistic)) +
+#  geom_histogram(bins = 40) + theme_bw() +
+#  scale_x_continuous(trans = "log2") +
+#  facet_wrap(~Annotation, nrow = 1) + 
+#  scale_fill_brewer(palette = 3, type = "qual") +
+#  labs(x = "Counts", y = "Segments, #")
+#
+#plt2 <- ggplot(Stat_data,
+#               aes(x = NegProbe, y = Q3, color = Annotation)) +
+#  geom_abline(intercept = 0, slope = 1, lty = "dashed", color = "darkgray") +
+#  geom_point() + guides(color = "none") + theme_bw() +
+#  scale_x_continuous(trans = "log2") + 
+#  scale_y_continuous(trans = "log2") +
+#  theme(aspect.ratio = 1) +
+#  labs(x = "Negative Probe GeoMean, Counts", y = "Q3 Value, Counts")
+#
+#plt3 <- ggplot(Stat_data,
+#               aes(x = NegProbe, y = Q3 / NegProbe, color = Annotation)) +
+#  geom_hline(yintercept = 1, lty = "dashed", color = "darkgray") +
+#  geom_point() + theme_bw() +
+#  scale_x_continuous(trans = "log2") + 
+#  scale_y_continuous(trans = "log2") +
+#  theme(aspect.ratio = 1) +
+#  labs(x = "Negative Probe GeoMean, Counts", y = "Q3/NegProbe Value, Counts")
+#
+#btm_row <- plot_grid(plt2, plt3, nrow = 1, labels = c("B", ""),
+#                     rel_widths = c(0.43,0.57))
+#plot_grid(plt1, btm_row, ncol = 1, labels = c("A", ""))
