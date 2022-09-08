72abdc2120092ef0503c438881fac102fa7a5ed4
George Zaki <george.zaki@nih.gov>
2022-09-08 15:09:33 -0400

    first implementation of study design function

--- R/study_design.R
+++ R/study_design.R
@@ -1,35 +1,28 @@
-####This code comes from 
+####This code comes from
 #http://www.bioconductor.org/packages/release/workflows/vignettes/GeoMxWorkflows/inst/doc/GeomxTools_RNA-NGS_Analysis.html#2_Getting_started
 
 
-library(NanoStringNCTools)
-library(GeomxTools)
-library(dplyr)
-library(ggforce)
-library(knitr)
-
-
 #' Ingest Nano String Digital Spatial Profile study
-#'
-#' @param dccfiles A character vector containing the paths to the DCC files.
+#' @param dccFiles A character vector containing the paths to the DCC files.
 #' @param pkcFiles A character vector representing the path to the corresponding PKC file
 #' @param phenoDataFile An optional character string representing the path to the corresponding phenotypic excel data file. It is recommended to use the Lab Worksheet in the exact order samples are provided in.
 #' @param phenoDataSheet An optional character string representing the excel sheet name containing the phenotypic data.
 #' @param phenoDataDccColName Character string identifying unique sample identifier column in phenoDataFile.
-#' @param phenoDataColPrefix  An optional prefix to add to the phenoData column names to distinguish them from the names of assayData matrices, featureData columns, and protocolData columns.
 #' @param protocolDataColNames  Character list of column names from phenoDataFile containing data about the experimental protocol or sequencing data.
 #' @param experimentDataColNames  Character list of column names from phenoDataFile containing data about the experiment's meta-data.
-#' 
-#' 
-#' @return A NanoString Object.
+#'
+#' @export
+#' @return A list containing the NanoString Object and the Sankey plot.
 
-study_design <- function(dccFiles, pkcFiles, phenoDataFile, phenoDataSheet = "Template", 
-                         phenoDataDccColName = "Sample_ID", 
-                         protocolDataColNames = c("aoi", "roi"), 
+
+
+study_design <- function(dccFiles, pkcFiles, phenoDataFile, phenoDataSheet = "Template",
+                         phenoDataDccColName = "Sample_ID",
+                         protocolDataColNames = c("aoi", "roi"),
                          experimentDataColNames = c("panel")) {
 
   # load data
-  demoData <-
+  study.data <-
     readNanoStringGeoMxSet(dccFiles = dccFiles,
                            pkcFiles = pkcFiles,
                            phenoDataFile = phenoDataFile,
@@ -39,7 +32,7 @@ study_design <- function(dccFiles, pkcFiles, phenoDataFile, phenoDataSheet = "Te
                            experimentDataColNames = experimentDataColNames)
 
 
-  pkcs <- annotation(demoData)
+  pkcs <- annotation(study.data)
   modules <- gsub(".pkc", "", pkcs)
   kable(data.frame(PKCs = pkcs, modules = modules))
 
@@ -48,7 +41,7 @@ study_design <- function(dccFiles, pkcFiles, phenoDataFile, phenoDataSheet = "Te
 
   # select the annotations we want to show, use `` to surround column names with
   # spaces or special symbols
-  count_mat <- count(pData(demoData), `slide name`, class, region, segment)
+  count_mat <- count(pData(study.data), `slide name`, class, region, segment)
   # simplify the slide names
   count_mat$`slide name` <- gsub("disease", "d",
                                  gsub("normal", "n", count_mat$`slide name`))
@@ -61,18 +54,18 @@ study_design <- function(dccFiles, pkcFiles, phenoDataFile, phenoDataSheet = "Te
     geom_parallel_sets(aes(fill = region), alpha = 0.5, axis.width = 0.1) +
     geom_parallel_sets_axes(axis.width = 0.2) +
     geom_parallel_sets_labels(color = "white", size = 5) +
-    theme_classic(base_size = 17) + 
+    theme_classic(base_size = 17) +
     theme(legend.position = "bottom",
           axis.ticks.y = element_blank(),
           axis.line = element_blank(),
           axis.text.y = element_blank()) +
-    scale_y_continuous(expand = expansion(0)) + 
+    scale_y_continuous(expand = expansion(0)) +
     scale_x_discrete(expand = expansion(0)) +
     labs(x = "", y = "") +
     annotate(geom = "segment", x = 4.25, xend = 4.25,
              y = 20, yend = 120, lwd = 2) +
     annotate(geom = "text", x = 4.19, y = 70, angle = 90, size = 5,
              hjust = 0.5, label = "100 segments")
-  
-  return(list("dsp_obj" = demoData, "plot" = p))
+
+  return(list("dsp.obj" = study.data, "plot" = p))
 }
