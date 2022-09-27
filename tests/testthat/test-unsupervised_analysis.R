e00ac2c0a94943819cb5eedcda5b2059d4a7df4d
Michalowski <michaloa@nciws-d2335-v.nci.nih.gov>
2022-09-27 17:37:13 -0400

    update function name, import

--- tests/testthat/test-unsupervised_analysis.R
+++ tests/testthat/test-unsupervised_analysis.R
@@ -1,19 +1,13 @@
-library(devtools)
-library(testthat)
-load_all()
-
-library(DSPWorkflow)
-library(Rtsne)
-library(umap)
-library(NanoStringNCTools)
-library(GeomxTools)
-
-
-load("/rstudio-files/ccr-dceg-data/users/aleksandra/DSPWorkflow/target.Data.rda") 
-output <- GeomxDimensionReduction(Object = target.Data, point.size = 1, point.alpha = 1)
-
 test_that("DSP object and dimension reduction plots returned", {
   
+  load("/rstudio-files/ccr-dceg-data/users/aleksandra/DSPWorkflow/target.Data.rda")
+  output <-
+    DimReduct(
+      Object = target.Data,
+      point.size = 1,
+      point.alpha = 1,
+      color.variable = "region"
+    )
   expected.elements = c("dsp.object", "plot.list")
   expect_equal(length(setdiff(expected.elements, names(output))), 0)
   
@@ -21,9 +15,23 @@ test_that("DSP object and dimension reduction plots returned", {
 
 test_that("dimension reductions present in DSP object", {
   
-  expect_true(sum(grepl("PC1|PC2", colnames(pData(output$dsp.object)))) == 2)
-  expect_true(sum(grepl("UMAP1|UMAP2", colnames(pData(output$dsp.object)))) == 2)
-  expect_true(sum(grepl("tSNE1|tSNE2", colnames(pData(output$dsp.object))))== 2)
+  load("/rstudio-files/ccr-dceg-data/users/aleksandra/DSPWorkflow/target.Data.rda")
+  output <-
+    DimReduct(
+      Object = target.Data,
+      point.size = 1,
+      point.alpha = 1,
+      color.variable = "region"
+    )
+  
+  expect_true(sum(grepl("PC1|PC2", colnames(
+    pData(output$dsp.object)
+  ))) == 2)
+  expect_true(sum(grepl("UMAP1|UMAP2", colnames(
+    pData(output$dsp.object)
+  ))) == 2)
+  expect_true(sum(grepl("tSNE1|tSNE2", colnames(
+    pData(output$dsp.object)
+  ))) == 2)
   
 })
-
