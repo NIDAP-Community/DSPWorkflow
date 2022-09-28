82f23eda54a384f3c93182f823375aded4b4c15f
George Zaki <george.zaki@nih.gov>
2022-09-28 15:50:41 -0400

    backing up spatial convolution code

--- R/spatial_deconvolution.R
+++ R/spatial_deconvolution.R
@@ -2,16 +2,34 @@
 #https://bioconductor.org/packages/release/bioc/vignettes/SpatialDecon/inst/doc/SpatialDecon_vignette_NSCLC.html
 
 
-#' Estimate cell composition across spatial samples using information from a reference matrix
+#' Estimate cell composition across spatial samples using information from a custom cell signature matrix
+
 #' @param dsp_qnorm normalized expression matrix (Gene x DSP_Samples).
 #' @param dsp_negnorm matrix of expected background for all data points in the normalized data matrix
 #' @param ref_mtx expression matrix (Gene x Reference_Samples) 
-#' @param ref_annot annotated data.frame with CellID and LabeledCellType
+#' @param ref_annot annotated data.frame with CellID and LabeledCellType, will be used along with ref_mtx to generate custom signature matrix
+#' @param CellID column of annotated data.frame containing cell barcode or name
+#' @param celltypeCol column of annotated data.frame containing cell identity information
+#' @param normalize scale profile matrix gene expression according to gene count
+#' @param minCellNum minimum number of cells (within a type) required to generate signature matrix
+#' @param minGenes filters cells according to minimum number of genes expressed 
+
 #' @export
 #' 
-#' @return A list containing the results of spatial deconvolution, res$beta contains matrix of estimated cell abundances, heatmap of estimated cell abundances
+#' @return A list res containing the results of spatial deconvolution, 
+#' res$beta: matrix of estimated cell abundances
+#' res$X: cell signature (gene x celltype) matrix used to generate deconvolution results
+#' res$yhat, res$resids: fitted values and log2-scale residuals from deconvolution, can be used to measure observations' goodness of fit
+#' res$prop_of_all: res$beta rescaled to proportion of celltype across each sample 
+#' heatmap / barplot of estimated cell abundances in data
 
-spatial_deconvolution <- function(dsp_qnorm, dsp_negnorm, ref_mtx, ref_annot){
+spatial_deconvolution <- function(dsp_qnorm, dsp_negnorm, ref_mtx, ref_annot,
+                                  CellID = "CellID",
+                                  cellTypeCol = "LabeledCellType",
+                                  normalize = FALSE,
+                                  minCellNum = 0,
+                                  minGenes = 10
+                                  ){
   library(SpatialDecon)
   library(GeomxTools)
   
@@ -29,14 +47,14 @@ spatial_deconvolution <- function(dsp_qnorm, dsp_negnorm, ref_mtx, ref_annot){
   custom_mtx <- create_profile_matrix(
     mtx = mtx_for_deconv,
     cellAnnots = annot,
-    cellTypeCol = "LabeledCellType",  # column containing cell type
-    cellNameCol = "CellID",           # column containing cell ID/name
+    cellTypeCol = cellTypeCol,  
+    cellNameCol = CellID,           
     matrixName = "custom_DSP_mtx", # name of final profile matrix
-    outDir = NULL,                    # path to desired output directory, set to NULL if matrix should not be written
-    normalize = FALSE,                # Should data be normalized? 
-    minCellNum = 0,                   # minimum number of cells of one type needed to create profile, exclusive
-    minGenes = 10,                    # minimum number of genes expressed in a cell, exclusive
-    scalingFactor = 5,                # what should all values be multiplied by for final matrix
+    outDir = NULL,                  # path to desired output directory, set to NULL if matrix should not be written
+    normalize = normalize,                
+    minCellNum = minCellNum,                  
+    minGenes = minGenes,              
+    scalingFactor = 1,              # what should all values be multiplied by for final matrix
     discardCellTypes = FALSE)
   
   # Run Spatial Deconvolution
@@ -45,8 +63,15 @@ spatial_deconvolution <- function(dsp_qnorm, dsp_negnorm, ref_mtx, ref_annot){
                       X = custom_mtx,
                       align_genes = TRUE)
   
-  # Plot heatmap of estimated cell fraction
+  # Heatmap of estimated cell fraction ~ res$beta
   print(heatmap(t(res$beta), cexCol = 0.5, cexRow = 0.7, margins = c(10,7)))
+  
+  # Heatmap of cell profile matrix used ~ res$X
+  print(heatmap(sweep(res$X, 1, apply(res$X, 1, max), "/"), labRow = NA, margins = c(10, 5)))
+  
+  # Abundance Barplot - draws legend in a new page 
+  print(TIL_barplot(res$prop_of_all, draw_legend = TRUE, cex.names = 0.3))
+  
   return(res)
 }
 
