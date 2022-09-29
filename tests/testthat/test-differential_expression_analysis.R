4cf6352bd2040056dc7685f9a09540e69e48d3ad
maggiec <maggie.cam@nih.gov>
2022-09-29 16:08:19 -0400

    Add test code

---
+++ tests/testthat/test-differential_expression_analysis.R
@@ -0,0 +1,49 @@
+test_that("Run Diff Exp Analysis", {
+    
+    load("/rstudio-files/ccr-dceg-data/users/maggie/DSPWorkflow/tests/testthat/fixtures/target.Data.rda")
+    data <- target.Data
+    data <- normalize(data, norm_method="quant")
+    goi <- c("CD274", "CD8A", "CD68", "EPCAM",
+             "KRT18", "NPHS1", "NPHS2", "CALB1", "CLDN8")
+    data <- target.Data[goi,]
+    groups <- c("DKD", "normal")
+    element = "log_q"
+    nCores = parallel::detectCores()
+    regions <- c("glomerulus", "tubule")
+    slideCol <- "slide name"
+    classCol <- "class"
+    multiCore = TRUE
+    pAdjust = NULL
+    pairwise = NULL
+    analysisType <- "Within Groups" 
+    fclim <- 1.5
+    
+    reslist.1 <- DiffExpr(data, element, analysisType, regions, 
+                          groups, slideCol, classCol, fclim,
+                          multiCore , nCores, pAdjust, pairwise)
+    grid.draw(reslist.1$sample_table)
+    grid.newpage()
+    grid.draw(reslist.1$summary_table)
+    expected.elements <- c("results","sample_table","summary_table")
+    expect_setequal(names(reslist.1),expected.elements)
+    
+    lfc.1 <- reslist.1$result %>% filter(Gene == "CALB1") %>% filter(Subset == "normal") %>% pull(Log_Fold_Change)
+    pval.1 <- reslist.1$result %>% filter(Gene == "CALB1") %>% filter(Subset == "normal") %>% pull(Pval)
+    expect_equal(lfc.1, -2.014,tolerance=1e-3)
+    expect_equal(pval.1, 0.0274,tolerance=1e-3)
+    
+    analysisType <- "Between Groups" 
+    reslist.2 <- DiffExpr(data, element, analysisType, regions, 
+                      groups, slideCol, classCol, fclim,
+                     multiCore , nCores, pAdjust, pairwise)
+    grid.draw(reslist.2$sample_table)
+    grid.newpage()
+    grid.draw(reslist.2$summary_table)
+    expected.elements <- c("results","sample_table","summary_table")
+    expect_setequal(names(reslist.2),expected.elements)
+    
+    lfc.2 <- reslist.2$result %>% filter(Gene == "CALB1") %>% filter(Subset == "tubule") %>% pull(Log_Fold_Change)
+    pval.2 <- reslist.2$result %>% filter(Gene == "CALB1") %>% filter(Subset == "tubule") %>% pull(Pval)
+    expect_equal(lfc.2, -1.408,tolerance=1e-3)
+    expect_equal(pval.2, 0.01268,tolerance=1e-3)
+})
