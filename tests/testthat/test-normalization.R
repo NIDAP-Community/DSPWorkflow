b8195d04a43743326c8f051ef49131a8e7a7299a
Highfill <highfillca@nciws-d2335-v.nci.nih.gov>
2022-09-30 14:08:20 -0400

    update

--- tests/testthat/test-normalization.R
+++ tests/testthat/test-normalization.R
@@ -1,8 +1,9 @@
 test_that("Normalization Success", {
-  load("/rstudio-files/ccr-dceg-data/users/Chad/target_demoData.Rdata") 
-  
-  dsp.list <- GeoMxNorm(Data = target_demoData, Norm = "quant")
+  #load("/rstudio-files/ccr-dceg-data/users/Chad/DSP/tests/testthat/fixtures/target_demoDataNorm.Rdata") 
+  target_demoDataNorm <- readRDS(test_path("fixtures", "target_demoDataNorm.rds"))
+ 
+  dsp.list <- GeoMxNorm(target_demoDataNorm, "quant")
+  #dsp.list <- GeoMxNorm(Data = target_demoData, Norm = "quant")
   expected.elements = c("plot", "boxplot")
   expect_equal(length(setdiff(expected.elements, names(dsp.list))), 0)
-  
 })
