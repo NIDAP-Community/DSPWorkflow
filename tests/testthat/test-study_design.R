9e4a6d035b68cd55b584ec1a34cd69130f30f6c6
Ned Cauley <ned.cauley@nih.gov>
2022-12-05 17:22:29 -0500

    Updated study design unit test

--- tests/testthat/test-study_design.R
+++ tests/testthat/test-study_design.R
@@ -1,15 +1,30 @@
 test_that("Load Kidney dataset", {
+  # The data directory containing all PKC, DCC, and annotation files
   datadir <- "/rstudio-files/ccr-dceg-data/data/WTA_NGS_Example"
+  
   DCCFiles <- dir(file.path(datadir, "dccs"), pattern = ".dcc$",
                   full.names = TRUE, recursive = TRUE)
-#  PKCFiles <- dir(file.path(datadir, "pkcs"), pattern = ".pkc",
-#              full.names = TRUE, recursive = TRUE)
+  
+  # Multiple PKC file grabber currently not functional
+  #PKCFiles <- dir(file.path(datadir, "pkcs"), pattern = ".pkc",
+  #            full.names = TRUE, recursive = TRUE)
+  
   PKCFiles <- "/rstudio-files/ccr-dceg-data/data/Kidney_Dataset//pkcs/TAP_H_WTA_v1.0.pkc"
+  
   SampleAnnotationFile <- dir(file.path(datadir, "annotation"), pattern = ".xlsx$",
                               full.names = TRUE, recursive = TRUE)
+  
+  # Optional parameters for selecting annotation column names that are used by the GeoMX object
+  DccColName <- "Sample_ID"
+  ProtocolColNames <- c("aoi", "roi")
+  ExperimentColNames = c("panel")
+  
 
   dsp.list <- StudyDesign(dccFiles = DCCFiles, pkcFiles = PKCFiles,
-                           phenoDataFile = SampleAnnotationFile)
+                           phenoDataFile = SampleAnnotationFile,
+                           phenoDataDccColName = DccColName,
+                           protocolDataColNames = ProtocolColNames,
+                           experimentDataColNames = ExperimentColNames)
   print(dsp.list$plot)
   expected.elements = c("plot", "dsp.obj")
   expect_setequal(names(dsp.list), expected.elements)
