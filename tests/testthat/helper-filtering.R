34888f1fa99a24cc20be9eae822e25dc8694543c
Ned Cauley <ned.cauley@nih.gov>
2023-02-01 18:23:49 -0500

    Updated pkc file path to fixtures

--- tests/testthat/helper-filtering.R
+++ tests/testthat/helper-filtering.R
@@ -4,25 +4,25 @@ select_dataset_filtering <- function(dataset) {
     
     print("selected kidney dataset") 
     object <- readRDS(test_path("fixtures/Human_Kidney", "qcHumanKidney.RDS"))
-    pkcFile <- "/rstudio-files/ccr-dceg-data/data/Kidney_Dataset/pkcs/TAP_H_WTA_v1.0.pkc"
+    pkcFile <- test_path("fixtures/Human_Kidney", "TAP_H_WTA_v1.0.pkc")
     
   } else if (dataset == "thymus"){
     
     print("selected thymus dataset")
     object <- readRDS(test_path("fixtures/Mouse_Thymus", "qcMouseThymus.RDS"))
-    pkcFile <- "/rstudio-files/ccr-dceg-data/data/Thymus_Dataset/pkcs/Mm_R_NGS_WTA_v1.0.pkc"
+    pkcFile <- test_path("fixtures/Mouse_Thymus", "Mm_R_NGS_WTA_v1.0.pkc")
     
   } else if (dataset == "colon"){
     
     print("selected colon dataset")
     object <- readRDS(test_path("fixtures/Human_Colon", "qcHumanColon.RDS"))
-    pkcFile <- "/rstudio-files/ccr-dceg-data/data/Colon_Dataset/pkcs/Hs_R_NGS_WTA_v1.0.pkc"
+    pkcFile <- test_path("fixtures/Human_Colon", "Hs_R_NGS_WTA_v1.0.pkc")
     
   } else if (dataset == "nsclc"){
     
     print("selected nsclc dataset")
     object <- readRDS(test_path("fixtures/Human_NSCLC", "qcHumanNSCLC.RDS"))
-    pkcFile <- "/rstudio-files/ccr-dceg-data/data/NSCLC_Dataset/pkcs/Hs_R_NGS_WTA_v1.0.pkc"
+    pkcFile <- test_path("fixtures/Human_NSCLC", "Hs_R_NGS_WTA_v1.0.pkc")
     
   }
   
