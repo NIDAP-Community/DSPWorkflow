51885cf46403a1e11264f1edb28fd8e2c669b25c
Ned Cauley <ned.cauley@nih.gov>
2023-02-08 11:36:02 -0500

    Update to heatmap test script

--- tests/testthat/test-heatmap.R
+++ tests/testthat/test-heatmap.R
@@ -1,3 +1,159 @@
+test_that("Test Human Kidney dataset", {
+  
+  kidney.dat <- select_normalized_RTD("kidney")
+  
+  dsp.list <- HeatMap(
+    ## Basic Parameters
+    kidney.dat, 
+    norm.method = "quant",
+    annotation.col = c("class", "segment", "region"),
+    ngenes = 500,
+    
+    ## Visualization
+    scale.by.row.or.col = "row",
+    show.rownames = FALSE,
+    show.colnames = FALSE,
+    
+    ## Clustering
+    clustering.method = "average",
+    cluster.rows = TRUE,
+    cluster.cols = TRUE,
+    clustering.distance.rows = "correlation",
+    clustering.distance.cols = "correlation",
+    annotation.row = NA,
+    #annotation.col = pData(target.data)[, c("class", "segment", "region")],
+    
+    ## Image
+    image.width = 3600,
+    image.height = 1800,
+    image.resolution = 300,
+    image.filename = "heatmap.clust.highCVgenes.png",
+    breaks.by.values = seq(-3, 3, 0.05), # 6/0.05=120 colors
+    heatmap.color = colorRampPalette(c("blue", "white", "red"))(120))  
+  
+  print(dsp.list$plot)
+  expected.elements = c("plot.genes", "plot")
+  expect_equal(length(setdiff(expected.elements, names(dsp.list))), 0)
+  
+})
+
+test_that("Test Colon Dataset", {
+  
+  colon.dat <- select_normalized_RTD("colon")
+  
+  dsp.list <- HeatMap(
+    ## Basic Parameters
+    colon.dat, 
+    norm.method = "quant",
+    annotation.col = c("class", "segment", "region"),
+    ngenes = 500,
+    
+    ## Visualization
+    scale.by.row.or.col = "row",
+    show.rownames = FALSE,
+    show.colnames = FALSE,
+    
+    ## Clustering
+    clustering.method = "average",
+    cluster.rows = TRUE,
+    cluster.cols = TRUE,
+    clustering.distance.rows = "correlation",
+    clustering.distance.cols = "correlation",
+    annotation.row = NA,
+    #annotation.col = pData(target.data)[, c("class", "segment", "region")],
+    
+    ## Image
+    image.width = 3600,
+    image.height = 1800,
+    image.resolution = 300,
+    image.filename = "heatmap.clust.highCVgenes.png",
+    breaks.by.values = seq(-3, 3, 0.05), # 6/0.05=120 colors
+    heatmap.color = colorRampPalette(c("blue", "white", "red"))(120))  
+  
+  print(dsp.list$plot)
+  expected.elements = c("plot.genes", "plot")
+  expect_equal(length(setdiff(expected.elements, names(dsp.list))), 0)
+  
+})
+
+test_that("Test Mouse Thymus Dataset", {
+  
+  thymus.dat <- select_normalized_RTD("thymus")
+  
+  dsp.list <- HeatMap(
+    ## Basic Parameters
+    thymus.dat, 
+    norm.method = "quant",
+    annotation.col = c("class", "segment", "region"),
+    ngenes = 500,
+    
+    ## Visualization
+    scale.by.row.or.col = "row",
+    show.rownames = FALSE,
+    show.colnames = FALSE,
+    
+    ## Clustering
+    clustering.method = "average",
+    cluster.rows = TRUE,
+    cluster.cols = TRUE,
+    clustering.distance.rows = "correlation",
+    clustering.distance.cols = "correlation",
+    annotation.row = NA,
+    #annotation.col = pData(target.data)[, c("class", "segment", "region")],
+    
+    ## Image
+    image.width = 3600,
+    image.height = 1800,
+    image.resolution = 300,
+    image.filename = "heatmap.clust.highCVgenes.png",
+    breaks.by.values = seq(-3, 3, 0.05), # 6/0.05=120 colors
+    heatmap.color = colorRampPalette(c("blue", "white", "red"))(120))  
+  
+  print(dsp.list$plot)
+  expected.elements = c("plot.genes", "plot")
+  expect_equal(length(setdiff(expected.elements, names(dsp.list))), 0)
+  
+})
+
+test_that("Test Human NSCLC Dataset", {
+  
+  nsclc.dat <- select_normalized_RTD("nsclc")
+  
+  dsp.list <- HeatMap(
+    ## Basic Parameters
+    nsclc.dat, 
+    norm.method = "quant",
+    annotation.col = c("class", "segment", "region"),
+    ngenes = 500,
+    
+    ## Visualization
+    scale.by.row.or.col = "row",
+    show.rownames = FALSE,
+    show.colnames = FALSE,
+    
+    ## Clustering
+    clustering.method = "average",
+    cluster.rows = TRUE,
+    cluster.cols = TRUE,
+    clustering.distance.rows = "correlation",
+    clustering.distance.cols = "correlation",
+    annotation.row = NA,
+    #annotation.col = pData(target.data)[, c("class", "segment", "region")],
+    
+    ## Image
+    image.width = 3600,
+    image.height = 1800,
+    image.resolution = 300,
+    image.filename = "heatmap.clust.highCVgenes.png",
+    breaks.by.values = seq(-3, 3, 0.05), # 6/0.05=120 colors
+    heatmap.color = colorRampPalette(c("blue", "white", "red"))(120))  
+  
+  print(dsp.list$plot)
+  expected.elements = c("plot.genes", "plot")
+  expect_equal(length(setdiff(expected.elements, names(dsp.list))), 0)
+  
+})
+
 test_that("making heatmap plot---Human Kidney, Mouse Thymus, Human Colon, NSCLC", {
 
 #    target.data <- readRDS(test_path("fixtures", "target.Data.rds"))  
