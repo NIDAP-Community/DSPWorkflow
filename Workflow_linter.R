0633b0bc64e7a2dd2f51a5e6f7468ed310bea06d
Rui He <her2@nih.gov>
2023-02-16 17:02:58 -0500

    Update the lint checker to chekc if variable are in dotted case or  lower case. Thus function name should alway produce warning as it shoudl eb lower camel. Line length limit is currently 120

--- Workflow_linter.R
+++ Workflow_linter.R
@@ -2,28 +2,8 @@
 
 lint(filename = File_to_lint, 
      linters = linters_with_defaults(
-       any_duplicated_linter(),
-       any_is_na_linter(),
-       backport_linter("oldrel-4", except = c("R_user_dir", "str2lang", "str2expression", "deparse1")),
-       consecutive_stopifnot_linter(),
-       expect_comparison_linter(),
-       expect_length_linter(),
-       expect_named_linter(),
-       expect_not_linter(),
-       expect_null_linter(),
-       expect_s3_class_linter(),
-       expect_s4_class_linter(),
-       expect_true_false_linter(),
-       expect_type_linter(),
-       implicit_integer_linter(),
+       commented_code_linter(),
        line_length_linter(120),
-       missing_argument_linter(),
-       nested_ifelse_linter(),
-       numeric_leading_zero_linter(),
-       paste_linter(),
-       redundant_ifelse_linter(),
-       sprintf_linter(),
-       strings_as_factors_linter(),
-       yoda_test_linter()
+       object_name_linter(styles = c("dotted.case", "lowercase", "camelCase"))
      )
 )
