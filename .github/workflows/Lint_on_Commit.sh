a4391d79a6abbdc38cb27d1de2b9dab3838fdb49
Rui He <her2@nih.gov>
2023-02-16 10:11:29 -0500

    Add lint test

--- .github/workflows/Lint_on_Commit.sh
+++ .github/workflows/Lint_on_Commit.sh
@@ -8,6 +8,7 @@ current_dir="$1"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 echo "Checking latest push to $current_branch"
 
+cp ./.github/workflows/.lintr ~/
 
 if [ -f DESCRIPTION ]; then
     echo "DESCRIPTION exist."
@@ -33,47 +34,22 @@ if [ -f DESCRIPTION ]; then
     
     echo -e "Tests to run as: \n${R_script_test[*]}\n"
     
-    R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));.libPaths();ip = as.data.frame(installed.packages()[,c(1,3:4)]);ip = ip[is.na(ip$Priority),1:2,drop=FALSE];ip;sessionInfo();'
-    
     for test_to_run in ${R_script_test[@]}
     do 
       
-      test_call='test_file("'"$current_dir"'/tests/testthat/'"$test_to_run"'");'
+      test_call='lint("'"$current_dir"'/tests/testthat/'"$test_to_run"'", style = "lintr");'
       echo "Running: "$test_call""
       
       echo "====================================================================="
       echo -e "Running $test_call"
       
-      R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));library(devtools);sink(file="'"${current_dir}"'/test.log");load_all();'"$test_call"'sink()'  
+      R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));library(lintr);sink(file="'"${current_dir}"'/lint.log";'"$test_call"'sink()'  
       
-      cat test.log
+      cat lint.log
       
       echo "====================================================================="
       echo "====================================================================="
       
-      message_check=$(tail -n 1 test.log | cut -d'|' -f 4 | cut -d' ' -f 2)
-      echo "Message Check: $message_check"
-      
-      if [ "$message_check" = "PASS" ]; then
-        FAIL_num=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 3)
-        if [ "$FAIL_num" != "0" ]; then
-          echo "Number of FAIL in test is $FAIL_num"
-          exit 2
-        else
-          echo "Passed Check!"
-        fi
-        
-      else
-        FAIL_num=$(tail -n3 test.log | head -n1 | cut -d'|' -f 1 | cut -d' ' -f 3)
-        
-        if [ "$FAIL_num" != "0" ]; then
-          echo "Number of FAIL in test is $FAIL_num"
-          exit 2
-        else
-          echo "Passed Check!"
-        fi
-      fi
-    done
 else 
     echo "DESCRIPTION file does not exist."
     exit 1
