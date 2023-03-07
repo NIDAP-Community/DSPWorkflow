4d7cae5ad5af4c3d8ce3cddef38d128bb2c47578
Ned Cauley <ned.cauley@nih.gov>
2023-03-07 16:51:39 -0500

    Updated study design R and test scripts according to code review feedback

--- R/study_design.R
+++ R/study_design.R
@@ -3,27 +3,27 @@
 # https://tinyurl.com/228bs2bp
 
 #' studyDesign: Study Design
-#' @description Combine Nanostring Digital Spatial Profile read count and 
+#' @description Combine Nanostring Digital Spatial Profile read count and
 #' annotation files into a GeoMX object
-#' @details `StudyDesign` returns a Sankey Plot and a GeoMX object. 
-#' For the function to run properly the annotation excel file must have the 
+#' @details `StudyDesign` returns a Sankey Plot and a GeoMX object.
+#' For the function to run properly the annotation excel file must have the
 #' specific field names: slide name', class, segment, region
 #' and the corresponding fields that can be renamed: Sample_ID, aoi, roi, panel
-#' 
-#' @param dccFiles A character vector containing the paths to the DCC files.
-#' @param pkcFiles A character vector representing the path to the 
+#'
+#' @param dcc.files A character vector containing the paths to the DCC files.
+#' @param pkc.files A character vector representing the path to the
 #' corresponding PKC file
-#' @param phenoDataFile An optional character string representing the path to 
-#' the corresponding phenotypic excel data file. It is recommended to use the 
+#' @param pheno.data.file An optional character string representing the path to
+#' the corresponding phenotypic excel data file. It is recommended to use the
 #' Lab Worksheet in the exact order samples are provided in.
-#' @param phenoDataSheet An optional character string representing the excel 
+#' @param pheno.data.sheet An optional character string representing the excel
 #' sheet name containing the phenotypic data.
-#' @param phenoDataDccColName Character string identifying unique sample 
+#' @param pheno.data.dcc.col.name Character string identifying unique sample
 #' identifier column in phenoDataFile.
-#' @param protocolDataColNames  Character list of column names from 
-#' phenoDataFile containing data about the experimental protocol or 
+#' @param protocol.data.col.names  Character list of column names from
+#' phenoDataFile containing data about the experimental protocol or
 #' sequencing data.
-#' @param experimentDataColNames  Character list of column names from 
+#' @param experiment.data.col.names  Character list of column names from
 #' phenoDataFile containing data about the experiment's meta-data.
 #'
 #' @importFrom GeomxTools readNanoStringGeoMxSet
@@ -45,70 +45,105 @@
 
 
 
-StudyDesign <- function(dcc.files, 
-                        pkc.files, 
-                        pheno.data.file, 
+studyDesign <- function(dcc.files,
+                        pkc.files,
+                        pheno.data.file,
                         pheno.data.sheet = "Template",
                         pheno.data.dcc.col.name = "Sample_ID",
                         protocol.data.col.names = c("aoi", "roi"),
                         experiment.data.col.names = c("panel")) {
-  
-  region <- segment <- x <- id <- y <- n <-NULL
+  region <- segment <- x <- id <- y <- n <- NULL
   `slide name` <- NULL
   # load all input data into a GeoMX object
-  study.data <-
-    readNanoStringGeoMxSet(dccFiles = dcc.files,
-                           pkcFiles = pkc.files,
-                           phenoDataFile = pheno.data.file,
-                           phenoDataSheet = pheno.data.sheet,
-                           phenoDataDccColName = pheno.data.dcc.col.name,
-                           protocolDataColNames = protocol.data.col.names,
-                           experimentDataColNames = experiment.data.col.names)
-
+  object <-
+    readNanoStringGeoMxSet(
+      dccFiles = dcc.files,
+      pkcFiles = pkc.files,
+      phenoDataFile = pheno.data.file,
+      phenoDataSheet = pheno.data.sheet,
+      phenoDataDccColName = pheno.data.dcc.col.name,
+      protocolDataColNames = protocol.data.col.names,
+      experimentDataColNames = experiment.data.col.names
+    )
+  
   
   # Review the PKC mapping files linked in the GeoMX object
-  pkcs <- annotation(study.data)
+  pkcs <- annotation(object)
   modules <- gsub(".pkc", "", pkcs)
   kable(data.frame(PKCs = pkcs, modules = modules))
   
   # Check each of the required fields for correct naming
   required.field.names = c("slide name", "class", "segment", "region")
-  given.field.names = colnames(sData(study.data))
-  for(field in required.field.names){
-    if(!(field %in% given.field.names)){
-      stop(paste0(field, " is required and NOT found in the annotation. 
-                  Please correct annotation sheet fields.\n"))
+  given.field.names = colnames(sData(object))
+  for (field in required.field.names) {
+    if (!(field %in% given.field.names)) {
+      stop(
+        paste0(
+          field,
+          " is required and NOT found in the annotation.
+                  Please correct annotation sheet fields.\n"
+        )
+      )
     }
   }
-
-
-
-  # select the annotations we want to show, use `` to surround column 
+  
+  
+  
+  # select the annotations we want to show, use `` to surround column
   # names with spaces or special symbols
-  count.mat <- count(pData(study.data), `slide name`, class, region, segment)
-
+  count.mat <-
+    count(pData(object), `slide name`, class, region, segment)
+  
   # gather the data and plot in order: class, slide name, region, segment
-  test.gr <- gather_set_data(count.mat, 1:4)
-  test.gr$x <- factor(test.gr$x,
-                      levels = c("class", "slide name", "region", "segment"))
-
+  # gather_set_data creates x, id, y, and n fields within sankey.count.data
+  sankey.count.data <- gather_set_data(count.mat, 1:4)
+  sankey.count.data$x <-
+    factor(
+      sankey.count.data$x,
+      levels = c("class", "slide name", "region", "segment")
+    )
+  
   # plot Sankey
-  p <- ggplot(test.gr, aes(x, id = id, split = y, value = n)) +
+  sankey.plot <-
+    ggplot(sankey.count.data,
+           aes(
+             x,
+             id = id,
+             split = y,
+             value = n
+           )) +
     geom_parallel_sets(aes(fill = region), alpha = 0.5, axis.width = 0.1) +
     geom_parallel_sets_axes(axis.width = 0.2) +
-    geom_parallel_sets_labels(color = "gray", size = 5, angle = 0) +
+    geom_parallel_sets_labels(color = "gray",
+                              size = 5,
+                              angle = 0) +
     theme_classic(base_size = 17) +
-    theme(legend.position = "bottom",
-          axis.ticks.y = element_blank(),
-          axis.line = element_blank(),
-          axis.text.y = element_blank()) +
+    theme(
+      legend.position = "bottom",
+      axis.ticks.y = element_blank(),
+      axis.line = element_blank(),
+      axis.text.y = element_blank()
+    ) +
     scale_y_continuous(expand = expansion(0)) +
     scale_x_discrete(expand = expansion(0)) +
     labs(x = "", y = "") +
-    annotate(geom = "segment", x = 4.25, xend = 4.25,
-             y = 20, yend = 120, lwd = 2) +
-    annotate(geom = "text", x = 4.19, y = 70, angle = 90, size = 5,
-             hjust = 0.5, label = "100 segments")
-
-  return(list("dsp.obj" = study.data, "plot" = p))
+    annotate(
+      geom = "segment",
+      x = 4.25,
+      xend = 4.25,
+      y = 20,
+      yend = 120,
+      lwd = 2
+    ) +
+    annotate(
+      geom = "text",
+      x = 4.19,
+      y = 70,
+      angle = 90,
+      size = 5,
+      hjust = 0.5,
+      label = "100 segments"
+    )
+  
+  return(list("object" = object, "sankey.plot" = sankey.plot))
 }
--- tests/testthat/helper-study_design.R
+++ tests/testthat/helper-study_design.R
@@ -1,45 +1,107 @@
 selectDatasetSD <- function(dataset) {
-  
-  if (dataset == "kidney"){
+  # Human kidney diabetes dataset from the vignette
+  if (dataset == "kidney") {
+    print("selected kidney dataset")
+    dcc.files <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/WTA_NGS_Example/",
+        "dccs"
+      ),
+      pattern = ".dcc$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pkc.files <-
+      test_path("fixtures/Human_Kidney", "TAP_H_WTA_v1.0.pkc")
+    pheno.data.file <-
+      "/rstudio-files/ccr-dceg-data/data/Kidney_Dataset/annotation/kidney_AOI_Annotations_all_vignette.xlsx"
+    pheno.data.sheet = "Template"
     
-    print("selected kidney dataset") 
-    dccFiles <- dir(file.path("/rstudio-files/ccr-dceg-data/data/WTA_NGS_Example/", "dccs"), pattern = ".dcc$", full.names = TRUE, recursive = TRUE)
-    pkcFiles <- test_path("fixtures/Human_Kidney", "TAP_H_WTA_v1.0.pkc")
-    phenoDataFile <- "/rstudio-files/ccr-dceg-data/data/WTA_NGS_Example/annotation/kidney_AOI_Annotations_all_vignette.xlsx"
-    phenoDataSheet = "Template"
-    
-  } else if (dataset == "thymus"){
-    
+    # Mouse thymus cancer dataset from PMID 36049655
+  } else if (dataset == "thymus") {
     print("selected thymus dataset")
-    dccFiles <- dir(file.path("/rstudio-files/ccr-dceg-data/data/Thymus_Dataset/", "dccs"), pattern = ".dcc$", full.names = TRUE, recursive = TRUE)
-    pkcFiles <- test_path("fixtures/Mouse_Thymus", "Mm_R_NGS_WTA_v1.0.pkc")
-    phenoDataFile <- "/rstudio-files/ccr-dceg-data/data/Thymus_Dataset/annotations/Thymus_Annotation_updated_3.xlsx"
-    phenoDataSheet = "Annotation"
+    dcc.files <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/Thymus_Dataset/",
+        "dccs"
+      ),
+      pattern = ".dcc$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pkc.files <-
+      test_path("fixtures/Mouse_Thymus", "Mm_R_NGS_WTA_v1.0.pkc")
+    pheno.data.file <-
+      "/rstudio-files/ccr-dceg-data/data/Thymus_Dataset/annotations/Thymus_Annotation_updated_3.xlsx"
+    pheno.data.sheet = "Annotation"
     
-  } else if (dataset == "colon"){
-    
+    # Human colon cancer dataset from Nanostring Spatial Organ Atlas
+  } else if (dataset == "colon") {
     print("selected colon dataset")
-    dccFiles <- dir(file.path("/rstudio-files/ccr-dceg-data/data/Colon_Dataset/", "dccs"), pattern = ".dcc$", full.names = TRUE, recursive = TRUE)
-    pkcFiles <- test_path("fixtures/Human_Colon", "Hs_R_NGS_WTA_v1.0.pkc")
-    phenoDataFile <- dir(file.path("/rstudio-files/ccr-dceg-data/data/Colon_Dataset/", "annotation"), pattern = ".xlsx$",
-                                full.names = TRUE, recursive = TRUE)
-    phenoDataSheet = "SegmentProperties"  
+    dcc.files <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/Colon_Dataset/",
+        "dccs"
+      ),
+      pattern = ".dcc$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pkc.files <-
+      test_path("fixtures/Human_Colon", "Hs_R_NGS_WTA_v1.0.pkc")
+    pheno.data.file <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/Colon_Dataset/",
+        "annotation"
+      ),
+      pattern = ".xlsx$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pheno.data.sheet = "SegmentProperties"
     
-  } else if (dataset == "nsclc"){
-    
+    # Human non-small cell lung carcinoma dataset from PMID 36216799
+  } else if (dataset == "nsclc") {
     print("selected nsclc dataset")
-    dccFiles <- dir(file.path("/rstudio-files/ccr-dceg-data/data/NSCLC_Dataset/", "dccs"), pattern = ".dcc$", full.names = TRUE, recursive = TRUE)
-    pkcFiles <- test_path("fixtures/Human_NSCLC", "Hs_R_NGS_WTA_v1.0.pkc")
-    phenoDataFile <- dir(file.path("/rstudio-files/ccr-dceg-data/data/NSCLC_Dataset/", "annotation"), pattern = ".xlsx$",
-                                full.names = TRUE, recursive = TRUE)
-    phenoDataSheet = "Template" 
+    dcc.files <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/NSCLC_Dataset/",
+        "dccs"
+      ),
+      pattern = ".dcc$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pkc.files <-
+      test_path("fixtures/Human_NSCLC", "Hs_R_NGS_WTA_v1.0.pkc")
+    pheno.data.file <- dir(
+      file.path(
+        "/rstudio-files/ccr-dceg-data/data/NSCLC_Dataset/",
+        "annotation"
+      ),
+      pattern = ".xlsx$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+    pheno.data.sheet = "Template"
     
   }
   
-  phenoDataDccColName <- "Sample_ID"
-  protocolDataColNames <- c("aoi", "roi")
-  experimentDataColNames = c("panel")
+  # Additional fields with no dataset-specific values
+  pheno.data.dcc.col.name <- "Sample_ID"
+  protocol.data.col.names <- c("aoi", "roi")
+  experiment.data.col.names = c("panel")
   
-  return(list("pkcFiles" = pkcFiles, "phenoDataFile" = phenoDataFile, "phenoDataSheet" = phenoDataSheet, "dccFiles" = dccFiles, "phenoDataDccColName" = phenoDataDccColName, "protocolDataColNames" = protocolDataColNames, "experimentDataColNames" = experimentDataColNames))
+  return(
+    list(
+      "pkc.files" = pkc.files,
+      "pheno.data.file" = pheno.data.file,
+      "pheno.data.sheet" = pheno.data.sheet,
+      "dcc.files" = dcc.files,
+      "pheno.data.dcc.col.name" = pheno.data.dcc.col.name,
+      "protocol.data.col.names" = protocol.data.col.names,
+      "experiment.data.col.names" = experiment.data.col.names
+    )
+  )
   
 }
