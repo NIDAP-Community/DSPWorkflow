d70011de82a18d509f2d102e47c2a0e026119a6e
Rui He <her2@nih.gov>
2023-03-07 13:37:03 -0500

    Update Helper script for select normalized

--- tests/testthat/helper-select_normalized_rtd.R
+++ tests/testthat/helper-select_normalized_rtd.R
@@ -1,27 +1,36 @@
-select_normalized_RTD <- function(dataset) {
+# Load an RTD object of normalized read data from the test fixtures 
+selectNormalizedRtd <- function(dataset) {
   
+  # Human kidney diabetes dataset from the vignette
   if (dataset == "kidney"){
     
     print("selected kidney dataset") 
-    inputObject <- readRDS(test_path("fixtures/Human_Kidney", "normalizationHumanKidney.RDS"))
-    
+    object <- readRDS(test_path("fixtures/Human_Kidney", 
+                                "normalizationHumanKidney.RDS"))
+  
+  # Mouse thymus cancer dataset from PMID 36049655
   } else if (dataset == "thymus"){
     
     print("selected thymus dataset")
-    inputObject <- readRDS(test_path("fixtures/Mouse_Thymus", "normalizationMouseThymus.RDS"))
-    
+    object <- readRDS(test_path("fixtures/Mouse_Thymus", 
+                                "normalizationMouseThymus.RDS"))
+  
+  # Human colon cancer dataset from Nanostring Spatial Organ Atlas
   } else if (dataset == "colon"){
     
     print("selected colon dataset")
-    inputObject <- readRDS(test_path("fixtures/Human_Colon", "normalizationHumanColon.RDS"))
-    
+    object <- readRDS(test_path("fixtures/Human_Colon", 
+                                "normalizationHumanColon.RDS"))
+  
+  # Human non-small cell lung carcinoma dataset from PMID 36216799
   } else if (dataset == "nsclc"){
     
     print("selected nsclc dataset")
-    inputObject <- readRDS(test_path("fixtures/Human_NSCLC", "normalizationHumanNSCLC.RDS"))
+    object <- readRDS(test_path("fixtures/Human_NSCLC", 
+                                "normalizationHumanNSCLC.RDS"))
     
   }
 
-  return(inputObject)
+  return(object)
   
 }
