037bcad617c48d60e4fffc51b9ae4494f3996c20
Highfill <highfillca@nciws-d2335-v.nci.nih.gov>
2023-03-15 16:40:07 -0400

    Clean Up

--- R/filtering.R
+++ R/filtering.R
@@ -2,7 +2,14 @@
 #http://www.bioconductor.org/packages/release/workflows/vignettes/GeoMxWorkflows/inst/doc/GeomxTools_RNA-NGS_Analysis.html#44_Limit_of_Quantification
 
 #' Normalize Nano String Digital Spatial Profile
-#' @param data A NanoStringGeoMxSet dataset
+#' 
+#' @title Filtering NanoStringGeoMxSet dataset
+#'
+#' @description Function for filtering NanoStringGeoMxSet dataset
+#'
+#' @details This function will run various filtering parameters for NanoStringGeoMxSet datasets
+#' 
+#' @param Data A NanoStringGeoMxSet dataset
 #' @importFrom scales percent
 #' @importFrom Biobase pData
 #' @importFrom Biobase fData
@@ -13,9 +20,7 @@
 
 # Function I dropped
 #importFrom Rmpfr pmax
-
-
-# To call function, must have data = raw target_demoData, dsp.obj = QC demoData, 
+# To call function, must have data = raw object, dsp.obj = QC demoData, 
 # loq.cutoff 2 is recommended, loq.min 2 is recommend, 
 # cut.segment = remove segments with less than 10% of the genes detected; .05-.1 recommended,
 # goi = goi (genes of interest). Must be a vector of genes (i.e c("PDCD1", "CD274")),
@@ -46,38 +51,38 @@ filtering <- function(data, pkcs, loq.cutoff, loq.min, cut.segment, goi) {
   
   # Collapse probes to gene targets
   #target_Data <- aggregateCounts(data)
-
+  
   # Calculate LOQ per module tested
-  LOQ <- data.frame(row.names = colnames(data))
+  loq <- data.frame(row.names = colnames(data))
   for(module in modules) {
     vars <- paste0(c("NegGeoMean_", "NegGeoSD_"),
                    module)
     if(all(vars[1:2] %in% colnames(pData(data)))) {
-      LOQ[, module] <-
+      loq[, module] <-
         pmax(loq.min,
              pData(data)[, vars[1]] * 
                pData(data)[, vars[2]] ^ loq.cutoff)
     }
   }
-  pData(data)$LOQ <- LOQ
+  pData(data)$loq <- loq
   
   ## 4.5.0 Filtering
-  LOQ_Mat <- c()
+  loq_mat <- c()
   for(module in modules) {
-    ind <- fData(data)$Module == module
-    Mat_i <- t(esApply(data[ind, ], MARGIN = 1,
+    ind <- fData(Data)$Module == module
+    mat_i <- t(esApply(Data[ind, ], MARGIN = 1,
                        FUN = function(x) {
                          x > LOQ[, module]
                        }))
-    LOQ_Mat <- rbind(LOQ_Mat, Mat_i)
+    loq_mat <- rbind(loq_mat, mat_i)
   }
   # ensure ordering since this is stored outside of the geomxSet
-  LOQ_Mat <- LOQ_Mat[fData(data)$TargetName, ]
-
+  loq_mat <- loq_mat[fData(data)$TargetName, ]
+  
   ##4.5.1S egment Gene Detection
   # Save detection rate information to pheno data
   pData(data)$GenesDetected <- 
-    colSums(LOQ_Mat, na.rm = TRUE)
+    colSums(loq_mat, na.rm = TRUE)
   pData(data)$GeneDetectionRate <-
     pData(data)$GenesDetected / nrow(data)
   
@@ -88,15 +93,15 @@ filtering <- function(data, pkcs, loq.cutoff, loq.min, cut.segment, goi) {
         labels = c("<1%", "1-5%", "5-10%", "10-15%", ">15%"))
   
   # stacked bar plot of different cut points (1%, 5%, 10%, 15%)
-  StackedBarPlot<- ggplot(pData(data),
-                          aes(x = DetectionThreshold)) +
-                   geom_bar(aes(fill = region)) +
-                   geom_text(stat = "count", aes(label = ..count..), vjust = -0.5) +
-                   theme_bw() +
-                   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
-                   labs(x = "Gene Detection Rate",
-                        y = "Segments, #",
-                        fill = "Segment Type")
+  stacked.bar.plot<- ggplot(pData(data),
+                            aes(x = DetectionThreshold)) +
+    geom_bar(aes(fill = region)) +
+    geom_text(stat = "count", aes(label = ..count..), vjust = -0.5) +
+    theme_bw() +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+    labs(x = "Gene Detection Rate",
+         y = "Segments, #",
+         fill = "Segment Type")
   
   
   # cut percent genes detected at 1, 5, 10, 15
@@ -104,11 +109,11 @@ filtering <- function(data, pkcs, loq.cutoff, loq.min, cut.segment, goi) {
   if(class(cut.segment)[1] != "numeric"){
     stop(paste0("Error: You have the wrong data class, must be numeric" ))
   }
-  target_demoData <- data[, pData(data)$GeneDetectionRate >= cut.segment]
+  object <- data[, pData(data)$GeneDetectionRate >= cut.segment]
   if(cut.segment > 1 | cut.segment < 0){
     stop(paste0("Error: You need perecentage in decimals between 0-1" ))
   }
-
+  
   # select the annotations we want to show, use `` to surround column names with
   # spaces or special symbols
   count_mat <- count(pData(data), `slide name`, class, region, segment)
@@ -121,29 +126,29 @@ filtering <- function(data, pkcs, loq.cutoff, loq.min, cut.segment, goi) {
   test_gr <- gather_set_data(count_mat, 1:4)
   test_gr$x <-factor(test_gr$x, levels = c("class", "slide name", "region", "segment"))
   # plot Sankey
-  Sankey_Plot<- ggplot(test_gr, aes(x, id = id, split = y, value = n)) +
-                geom_parallel_sets(aes(fill = region), alpha = 0.5, axis.width = 0.1) +
-                geom_parallel_sets_axes(axis.width = 0.2) +
-                geom_parallel_sets_labels(color = "white", size = 5) +
-                theme_classic(base_size = 17) + 
-                theme(legend.position = "bottom",
-                      axis.ticks.y = element_blank(),
-                      axis.line = element_blank(),
-                      axis.text.y = element_blank()) +
-                scale_y_continuous(expand = expansion(0)) + 
-                scale_x_discrete(expand = expansion(0)) +
-                labs(x = "", y = "") +
-                annotate(geom = "segment", x = 4.25, xend = 4.25, y = 20, 
-                         yend = 120, lwd = 2) +
-                annotate(geom = "text", x = 4.19, y = 70, angle = 90, size = 5,
-                         hjust = 0.5, label = "100 segments")
+  sankey.plot<- ggplot(test_gr, aes(x, id = id, split = y, value = n)) +
+    geom_parallel_sets(aes(fill = region), alpha = 0.5, axis.width = 0.1) +
+    geom_parallel_sets_axes(axis.width = 0.2) +
+    geom_parallel_sets_labels(color = "white", size = 5) +
+    theme_classic(base_size = 17) + 
+    theme(legend.position = "bottom",
+          axis.ticks.y = element_blank(),
+          axis.line = element_blank(),
+          axis.text.y = element_blank()) +
+    scale_y_continuous(expand = expansion(0)) + 
+    scale_x_discrete(expand = expansion(0)) +
+    labs(x = "", y = "") +
+    annotate(geom = "segment", x = 4.25, xend = 4.25, y = 20, 
+             yend = 120, lwd = 2) +
+    annotate(geom = "text", x = 4.19, y = 70, angle = 90, size = 5,
+             hjust = 0.5, label = "100 segments")
   
   ##4.5.2 Gene Detection Rate
   # Calculate detection rate:
-  LOQ_Mat <- LOQ_Mat[, colnames(target_demoData)]
-  fData(target_demoData)$DetectedSegments <- rowSums(LOQ_Mat, na.rm = TRUE)
-  fData(target_demoData)$DetectionRate <-
-    fData(target_demoData)$DetectedSegments / nrow(pData(target_demoData))
+  loq_mat <- loq_mat[, colnames(object)]
+  fData(object)$DetectedSegments <- rowSums(loq_mat, na.rm = TRUE)
+  fData(object)$DetectionRate <-
+    fData(object)$DetectedSegments / nrow(pData(object))
   
   # Gene of interest detection table
   goi <- goi
@@ -151,41 +156,41 @@ filtering <- function(data, pkcs, loq.cutoff, loq.min, cut.segment, goi) {
     stop(paste0("Error: You have the wrong data class, must be character vector" ))
   }
   goi_df <- data.frame(Gene = goi,
-    Number = fData(target_demoData)[goi, "DetectedSegments"],
-    DetectionRate = percent(fData(target_demoData)[goi, "DetectionRate"]))
+                       Number = fData(object)[goi, "DetectedSegments"],
+                       DetectionRate = percent(fData(object)[goi, "DetectionRate"]))
   
   ## 4.5.3 Gene Filtering
   # Plot detection rate:
   plot_detect <- data.frame(Freq = c(1, 5, 10, 20, 30, 50))
   plot_detect$Number <-
     unlist(lapply(c(0.01, 0.05, 0.1, 0.2, 0.3, 0.5),
-                  function(x) {sum(fData(target_demoData)$DetectionRate >= x)}))
-  plot_detect$Rate <- plot_detect$Number / nrow(fData(target_demoData))
+                  function(x) {sum(fData(object)$DetectionRate >= x)}))
+  plot_detect$Rate <- plot_detect$Number / nrow(fData(object))
   rownames(plot_detect) <- plot_detect$Freq
   
-  GenesDected<- ggplot(plot_detect, aes(x = as.factor(Freq), y = Rate, fill = Rate)) +
-                geom_bar(stat = "identity") +
-                geom_text(aes(label = formatC(Number, format = "d", big.mark = ",")),
-                          vjust = 1.6, color = "black", size = 4) +
-                scale_fill_gradient2(low = "orange2", mid = "lightblue",
-                                     high = "dodgerblue3", midpoint = 0.65,
-                                     limits = c(0,1),
-                                     labels = scales::percent) +
-                theme_bw() +
-                scale_y_continuous(labels = scales::percent, limits = c(0,1),
-                                   expand = expansion(mult = c(0, 0))) +
-                labs(x = "% of Segments",
-                     y = "Genes Detected, % of Panel > LOQ")
+  genes.detected<- ggplot(plot_detect, aes(x = as.factor(Freq), y = Rate, fill = Rate)) +
+    geom_bar(stat = "identity") +
+    geom_text(aes(label = formatC(Number, format = "d", big.mark = ",")),
+              vjust = 1.6, color = "black", size = 4) +
+    scale_fill_gradient2(low = "orange2", mid = "lightblue",
+                         high = "dodgerblue3", midpoint = 0.65,
+                         limits = c(0,1),
+                         labels = scales::percent) +
+    theme_bw() +
+    scale_y_continuous(labels = scales::percent, limits = c(0,1),
+                       expand = expansion(mult = c(0, 0))) +
+    labs(x = "% of Segments",
+         y = "Genes Detected, % of Panel > loq")
   
   # Subset to target genes detected in at least 10% of the samples.
   #   Also manually include the negative control probe, for downstream use
-  negativeProbefData <- subset(fData(target_demoData), CodeClass == "Negative")
+  negativeProbefData <- subset(fData(object), CodeClass == "Negative")
   neg_probes <- unique(negativeProbefData$TargetName)
-  target_demoData <- target_demoData[fData(target_demoData)$DetectionRate >= 0.1 |
-                      fData(target_demoData)$TargetName %in% neg_probes, ]
+  object <- object[fData(object)$DetectionRate >= 0.1 |
+                     fData(object)$TargetName %in% neg_probes, ]
   
   # retain only detected genes of interest
-  goi <- goi[goi %in% rownames(target_demoData)]
-
-  return(list("Stacked Bar Plot" = StackedBarPlot, "Table of Cuts" = tab, "Sankey Plot" = Sankey_Plot, "Genes Deccted Plot" = GenesDected, "target_demoData Dataset" = target_demoData))
+  goi <- goi[goi %in% rownames(object)]
+  
+  return(list("Stacked Bar Plot" = stacked.bar.plot, "Table of Cuts" = tab, "Sankey Plot" = Sankey.plot, "Genes Detected Plot" = genes.detected, "object Dataset" = object))
 }
