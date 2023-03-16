dc9149a98a5ce2cafb0c3ecba8f14d26d89b96a9
wangdi2016 <wangdi@mail.nih.gov>
2023-03-15 23:43:44 -0400

    Updates

--- tests/testthat/test-heatmap.R
+++ tests/testthat/test-heatmap.R
@@ -41,23 +41,3 @@ test_that("making heatmap plot---Test Human NSCLC Dataset", {
   expect_setequal(names(res), expected.elements)
   
 })
-
-test_that("making heatmap plot---with wrong ngenes", {
-  
-  data <- getDataset("nsclc")
-  data$ngenes <- 1
-  
-  expect_error(do.call(heatMap, data),
-               fixed = TRUE,
-               "ngenes must be interger > 1.\n")
-})
-
-test_that("making heatmap plot---with wrong ngenes", {
-  
-  data <- getDataset("nsclc")
-  data$ngenes <- "TP53"
-  
-  expect_error(do.call(heatMap, data),
-               fixed = TRUE,
-               "ngenes must be interger > 1.\n")
-})
