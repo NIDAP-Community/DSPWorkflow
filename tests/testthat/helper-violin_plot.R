308f8c85a74341b701fd2212720fc957de26ce35
bianjh <bianjh@nih.gov>
2023-04-10 15:38:54 -0400

    commit change before merge

--- tests/testthat/helper-violin_plot.R
+++ tests/testthat/helper-violin_plot.R
@@ -9,7 +9,7 @@ getViolinParam <- function(data) {
     
   } else if (data == "thymus"){
     
-    object = selectNormalizedRtd("thymus")
+    object = select_normalized_RTD("thymus")
     expr.type = "q_norm"
     genes = c("B2m","Sox9","Cd14","Epcam","Krt19","Cd8a","Cd4","FoxP3")
     group = "class"
@@ -17,7 +17,7 @@ getViolinParam <- function(data) {
     
   } else if (data == "colon"){
     
-    object = selectNormalizedRtd("colon")
+    object = select_normalized_RTD("colon")
     expr.type = "q_norm"
     genes = c("B2M","SOX9","CD14","EPCAM","KRT19","CD8A","CD4","FOXP3")
     group = "class"
@@ -25,7 +25,7 @@ getViolinParam <- function(data) {
     
   } else if (data == "nsclc"){
     
-    object = selectNormalizedRtd("nsclc")
+    object = select_normalized_RTD("nsclc")
     expr.type = "q_norm"
     genes = c("B2M","SOX9","CD14","EPCAM","KRT19","CD8A","CD4","FOXP3")
     group = "class"
