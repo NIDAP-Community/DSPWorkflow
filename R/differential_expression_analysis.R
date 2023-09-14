e9957334696f3b0e40cb99597b94ac54a0d43e0f
Ned Cauley <ned.cauley@nih.gov>
2023-09-14 17:18:12 -0400

    feat(DEG): Added linear model (no random effect) in DEG analysis and adjusted output table

--- R/differential_expression_analysis.R
+++ R/differential_expression_analysis.R
@@ -66,11 +66,91 @@ diffExpr <- function(object,
                      element = "q_norm",
                      multi.core = TRUE,
                      n.cores = 1,
-                     p.adjust = "BH",
+                     p.adjust = "fdr",
                      pairwise = TRUE,
                      fc.lim = 1.2,
                      pval.lim.1 = 0.05,
                      pval.lim.2 = 0.01) {
+  
+  ###
+  ### Helper functions
+  ###
+  
+  # Alternative function for running liner model when # slide = 1
+  # https://github.com/lardenoije/GeomxTools/blob/master/R/modelDE.R
+  modelDE <- function (object, elt, modelFormula, groupVar, 
+                       nCores, multiCore, pAdjust) 
+  {
+    if (is.null(modelFormula)) {
+      modelFormula <- design(object)
+    }
+    mTerms <- all.vars(modelFormula)
+    if ("1" %in% mTerms) {
+      mTerms <- mTerms[which(!(mTerms %in% "1"))]
+    }
+    if (!groupVar %in% mTerms) {
+      stop("Error: groupVar needs to be defined as fixed effect in the model.\n")
+    }
+    if (any(!mTerms %in% names(sData(object)))) {
+      stop("Error: Not all terms in the model formula are in pheno or protocol data.\n")
+    }
+    pDat <- sData(object)[, mTerms, drop=FALSE]
+    for (i in names(pDat)) {
+      if (inherits(i, "character")) {
+        pDat[, i] <- as.factor(pDat[, i])
+      }
+    }
+    if (nCores > 1) {
+      deFunc <- function(i, groupVar, pDat, modelFormula, exprs) {
+        dat <- data.frame(expr = exprs$exprs[i, ], pDat)
+        lmOut <- suppressWarnings(summary(lm(modelFormula, dat))$coefficients)
+        lmOut <- matrix(cbind(lmOut[-1, "Estimate", drop=FALSE], lmOut[-1, "Pr(>|t|)", drop=FALSE]), 
+                        ncol = 2, dimnames = list(gsub(groupVar, "", rownames(lmOut)[-1]), 
+                                                  c("Estimate", "Pr(>|t|)")))
+        return(lmOut)
+      }
+      exprs <- new.env()
+      exprs$exprs <- assayDataElement(object, elt = elt)
+      if (multiCore & Sys.info()["sysname"] != "Windows") {
+        lmOut <- parallel::mclapply(featureNames(object),
+                                    deFunc, groupVar, pDat, 
+                                    formula(paste("expr", as.character(modelFormula)[2], sep = " ~ ")),
+                                    exprs, mc.cores = nCores)
+      }
+      else {
+        cl <- parallel::makeCluster(getOption("cl.cores", nCores))
+        lmOut <- parallel::parLapply(cl, featureNames(object), deFunc, groupVar, pDat, 
+                                     formula(paste("expr", as.character(modelFormula)[2], sep = " ~ ")),
+                                     exprs)
+        suppressWarnings(parallel::stopCluster(cl))
+      }
+      lmOut <- do.call("rbind", lmOut)
+      rownames(lmOut) <- featureNames(object)
+    }
+    else {
+      deFunc <- function(expr, groupVar, pDat, modelFormula) {
+        dat <- data.frame(expr = expr, pDat)
+        lmOut <- suppressWarnings(summary(lm(modelFormula, dat))$coefficients)
+        lmOut <- matrix(cbind(lmOut[-1, "Estimate", drop=FALSE], lmOut[-1, "Pr(>|t|)", drop=FALSE]), 
+                        ncol = 2, dimnames = list(gsub(groupVar, "", rownames(lmOut)[-1]), 
+                                                  c("Estimate", "Pr(>|t|)")))
+        return(lmOut)
+      }
+      lmOut <- t(assayDataApply(object, 1, deFunc, groupVar, pDat, 
+                                formula(paste("expr", as.character(modelFormula)[2], sep = " ~ ")), 
+                                elt = elt))
+    }
+    if (!is.null(pAdjust)) {
+      lmOut <- cbind(lmOut, p.adjust(lmOut[, 2], method = pAdjust))
+    }
+    colnames(lmOut) <- c("Estimate", "Pr(>|t|)", pAdjust)
+    return(lmOut)
+  }
+  
+  
+  ###
+  ### Start of main function
+  ###
   
   # Check the number of cores available for the current machine
   available.cores <- detectCores()
@@ -81,7 +161,7 @@ diffExpr <- function(object,
     n.cores <- available.cores
   }
   
-  testClass <- testRegion <- Gene <- Subset <- NULL
+  main.group <- sub.group <- Gene <- Subset <- NULL
   
   # convert test variables to factors after checking input
   reg.check <- regions[!regions %in% Biobase::pData(object)[[region.col]]]
@@ -91,7 +171,7 @@ diffExpr <- function(object,
   
   regions <- regions[regions %in% Biobase::pData(object)[[region.col]]]
   regions <- factor(regions, levels = c(regions[1], regions[2]))
-  Biobase::pData(object)$testRegion <-
+  Biobase::pData(object)$sub.group <-
     factor(Biobase::pData(object)[[region.col]], levels = regions)
   
   grp.check <- groups[!groups %in% Biobase::pData(object)[[group.col]]]
@@ -101,7 +181,7 @@ diffExpr <- function(object,
   
   groups <- groups[groups %in% Biobase::pData(object)[[group.col]]]
   groups <- factor(groups, levels = c(groups[1], groups[2]))
-  Biobase::pData(object)$testClass <-
+  Biobase::pData(object)$main.group <-
     factor(Biobase::pData(object)[[group.col]], levels = groups)
   
   Biobase::pData(object)$slide <- factor(Biobase::pData(object)[[slide.col]])
@@ -117,20 +197,20 @@ diffExpr <- function(object,
   param.na <- names(ind.na[ind.na > 0])
   
   if (length(param.na) > 0) {
-    if (param.na[1] == "testRegion") {
+    if (param.na[1] == "sub.group") {
       regdiff <-
         setdiff(unique(Biobase::pData(object)[[region.col]]),
-                unique(levels(Biobase::pData(object)$testRegion)))
+                unique(levels(Biobase::pData(object)$sub.group)))
       regdiff <- paste0(regdiff, collapse = ", ")
       cat(
         sprintf(
           "At least one of the regions within the Region Column was not selected
             and is excluded: %s\n", regdiff)
       )
-    } else if (param.na[1] == "testClass") {
+    } else if (param.na[1] == "main.group") {
       classdiff <-
         setdiff(unique(Biobase::pData(object)[[group.col]]),
-                unique(levels(Biobase::pData(object)$testClass)))
+                unique(levels(Biobase::pData(object)$main.group)))
       classdiff <- paste0(classdiff, collapse = ", ")
       cat(
         sprintf(
@@ -142,137 +222,271 @@ diffExpr <- function(object,
   
   #Print Metadata Pivot Table
   metadata <- Biobase::pData(object) %>% rownames_to_column("sample")
-  met.tab <- metadata %>% select(testClass, testRegion, sample, slide) 
-  met.sum <- met.tab %>% group_by(testClass, testRegion, slide) %>% count() 
+  met.tab <- metadata %>% select(main.group, sub.group, sample, slide) 
+  met.sum <- met.tab %>% group_by(main.group, sub.group, slide) %>% count() 
   met.pivot <- met.sum %>% pivot_wider(names_from = slide, values_from = n) 
   #replace str_wrap(colnames(met.pivot), 10) below
   colnames(met.pivot) <- sapply(strsplit(colnames(met.pivot)," "),paste,collapse = "\n")
-  ind <- !(is.na(met.pivot$testClass) | is.na(met.pivot$testRegion))
+  ind <- !(is.na(met.pivot$main.group) | is.na(met.pivot$sub.group))
   met.pivot <- met.pivot[ind,]
   grid.newpage()
   gt <- tableGrob(met.pivot, theme = ttheme_default(base_size = 8))
   
   #Check for numbers of groups and regions listed for comparison
   
-  reg.col <- unique(Biobase::pData(object)$testRegion)
+  reg.col <- unique(Biobase::pData(object)$sub.group)
   reg.length <- length(reg.col[!is.na(reg.col)])
-  grp.col <- unique(Biobase::pData(object)$testClass)
+  grp.col <- unique(Biobase::pData(object)$main.group)
   grp.length <- length(grp.col[!is.na(grp.col)])
   
   #Run DEG Analysis
   options(digits = 3)
   
+  # Check if the slide has greater than 1 for mixed model
+  
+  # Reset method flag 
+  stat.method <- NULL
+  
   if (analysis.type == "Within Groups") {
     cat("Running Within Group Analysis between Regions\n")
     if (reg.length < 2) {
-      if ("testRegion" %in% param.na) {
+      if ("sub.group" %in% param.na) {
         stop("Cannot run Within Group Analysis with 1 Region.\n")
       }
     }
     title1 <- "DEG lists from within slide contrast:"
     results <- c()
     for (status in groups) {
-      ind <- Biobase::pData(object)$testClass == status
+      ind <- Biobase::pData(object)$main.group == status
       ind[is.na(ind)] <- FALSE
-      ind2 <- Biobase::pData(object)$testRegion %in% regions
+      ind2 <- Biobase::pData(object)$sub.group %in% regions
       ind2[is.na(ind2)] <- FALSE
       #Check to see if there are 2 regions for comparison:
       object.sub <- object[, ind & ind2]
-      r <- length(unique(pData(object.sub)$testRegion))
+      r <- length(unique(pData(object.sub)$sub.group))
       cat(sprintf("Number of regions in group %s: %s \n", status,r))
       if(r < 2){
         stop("There are not enough sample regions to do the analysis")
       } else {
-        mixed.out <- mixedModelDE(
-          object.sub,
-          elt = element,
-          modelFormula = ~ testRegion + (1 + testRegion |
-                                           slide),
-          groupVar = "testRegion",
-          multiCore = multi.core,
-          nCores = n.cores,
-          pAdjust = p.adjust,
-          pairwise = pairwise
-        )
+        
+        # Check if there are enough slides for mixed effect model
+        slide.list <- unique(pData(object.sub)$slide)
+        
+        if(length(slide.list) > 1) {
+          print("Running DE with linear mixed-effect model")
+          
+          stat.method <- "mixed-effect model"
+          
+          mixed.out <- mixedModelDE(
+            object.sub,
+            elt = element,
+            modelFormula = ~ sub.group + (1 + sub.group |
+                                            slide),
+            groupVar = "sub.group",
+            multiCore = multi.core,
+            nCores = n.cores,
+            pAdjust = p.adjust,
+            pairwise = pairwise
+          )
+        } else {
+          print("Running DE with linear model (no mixed-effect)")
+          warning("The random effect from slide to slide variance will not be considered.
+                  This should only be used for exploratory purposes.")
+          
+          stat.method <- "linear model"
+          
+          lm.out <- modelDE(
+            object = object.sub, 
+            elt = element, 
+            modelFormula = ~ sub.group, 
+            groupVar = "sub.group", 
+            nCores = n.cores, 
+            multiCore = multi.core, 
+            pAdjust = "fdr"
+          )
+        }
       }
       
-      # format results as data.frame
-      test.results <- do.call(rbind, mixed.out["lsmeans", ])
-      tests <- rownames(test.results)
-      test.results <- as.data.frame(test.results)
-      test.results$Contrast <- tests
-      test.results$Gene <-
-        unlist(lapply(colnames(mixed.out),
-                      rep, nrow(mixed.out["lsmeans", ][[1]])))
-      test.results$Subset <- status
-      test.results$FDR <-
-        p.adjust(test.results$`Pr(>|t|)`, method = "fdr")
-      test.results <-
-        test.results[, c("Gene",
-                         "Subset",
-                         "Contrast",
-                         "Estimate",
-                         "Pr(>|t|)",
-                         "FDR")]
+      # Perform formatting specific to analysis type
+      if(stat.method == "mixed-effect model") {
+        # Formatting specific to the linear mixed model output
+        
+        # Format results as data.frame
+        test.results <- do.call(rbind, mixed.out["lsmeans", ])
+        
+        # Rename pval column
+        colnames(test.results)[2] <- "pval"
+        
+        # Create a column for contrast
+        test.results <- as.data.frame(test.results)
+        test.results$Contrast <- paste0(regions[1],"_",regions[2])
+        
+        # Create a column for gene
+        test.results$Gene <-
+          unlist(lapply(colnames(mixed.out),
+                        rep, nrow(mixed.out["lsmeans", ][[1]])))
+        
+        # Create a column for the group
+        test.results$Subset <- status
+        
+        # Run FDR multiple test correction
+        test.results$FDR <-
+          p.adjust(test.results$pval, method = "fdr")
+        
+        # Reorganize columns of final results
+        test.results <-
+          test.results[, c("Gene",
+                           "Subset",
+                           "Contrast",
+                           "Estimate",
+                           "pval",
+                           "FDR")]
+        
+      } else if(stat.method == "linear model"){
+        # Formatting specific to the linear model output (no mixed effect)
+        
+        # Correct pval and adjpval column name
+        colnames(lm.out)[2] <- "pval"
+        colnames(lm.out)[3] <- "FDR"
+        
+        # Correct pval column name and convert to dataframe 
+        test.results <- data.frame(lm.out)
+        
+        # Create a column for contrast
+        test.results$Contrast <- paste0(regions[1],"_",regions[2])
+        
+        # Create a column for gene and remove gene rownames
+        test.results$Gene <- row.names(test.results)
+        rownames(test.results) <- NULL
+        
+        # Create a column for the group
+        test.results$Subset <- status
+        
+        # Reorganize columns of final results
+        test.results <-
+          test.results[, c("Gene",
+                           "Subset",
+                           "Contrast",
+                           "Estimate",
+                           "pval",
+                           "FDR")]
+      } else {
+        stop("No valid statistics method was available for the data")
+      }
+
+      # Add results of current contrast to overall results
       results <- rbind(results, test.results)
     }
   } else {
     cat("Running Between Group Analysis for Regions\n")
     if (grp.length < 2) {
-      if ("testClass" %in% param.na) {
+      if ("main.group" %in% param.na) {
         stop("Cannot run Between Group Analysis with 1 Group.\n")
       }
     }
     title1 <- "DEG lists from Between Slides contrast:"
     results <- c()
     for (region in regions) {
-      ind <- Biobase::pData(object)$testRegion == region
+      ind <- Biobase::pData(object)$sub.group == region
       ind[is.na(ind)] <- FALSE
-      ind2 <- Biobase::pData(object)$testClass %in% groups
+      ind2 <- Biobase::pData(object)$main.group %in% groups
       ind2[is.na(ind2)] <- FALSE
       object.sub <- object[, ind & ind2]
       #Check to see if there are 2 groups for comparison:
-      r <- length(unique(pData(object.sub)$testClass))
+      r <- length(unique(pData(object.sub)$main.group))
       cat(sprintf("Number of groups in region %s: %s \n", region, r))
       if(r < 2){
         stop("There are not enough sample groups to do the analysis")
       } else {
+        
+        print("Running DE with linear mixed-effect model")
+        
+        stat.method <- "mixed-effect model"
+        
         mixed.out <-
           mixedModelDE(
             object.sub,
             elt = element,
-            modelFormula = ~ testClass + (1 | slide),
-            groupVar = "testClass",
+            modelFormula = ~ main.group + (1 | slide),
+            groupVar = "main.group",
             multiCore = multi.core,
             nCores = n.cores,
             pAdjust = p.adjust,
             pairwise = pairwise
           )
       }
+    }
+    
+    # Perform formatting specific to analysis type
+    if(stat.method == "mixed-effect model") {
+      # Formatting specific to the linear mixed model output
       
-      # format results as data.frame
+      # Format results as data.frame
       test.results <- do.call(rbind, mixed.out["lsmeans", ])
-      tests <- rownames(test.results)
-      test.results <- as.data.frame(test.results)
-      test.results$Contrast <- tests
       
-      # use lapply in case you have multiple levels of your test factor to
-      # correctly associate gene name with it's row in the results table
+      # Rename pval column
+      colnames(test.results)[2] <- "pval"
+      
+      # Create a column for contrast and remove contrast row names
+      test.results <- as.data.frame(test.results)
+      test.results$Contrast <- paste0(groups[1],"_",groups[2])
+      rownames(test.results) <- NULL
+      
+      # Create a column for gene
       test.results$Gene <-
         unlist(lapply(colnames(mixed.out),
                       rep, nrow(mixed.out["lsmeans", ][[1]])))
+      
+      # Create a column for the group
       test.results$Subset <- region
+      
+      # Run FDR multiple test correction
       test.results$FDR <-
-        p.adjust(test.results$`Pr(>|t|)`, method = "fdr")
+        p.adjust(test.results$pval, method = "fdr")
+      
+      # Reorganize columns of final results
       test.results <-
         test.results[, c("Gene",
                          "Subset",
                          "Contrast",
                          "Estimate",
-                         "Pr(>|t|)",
+                         "pval",
                          "FDR")]
-      results <- rbind(results, test.results)
+      
+    } else if(stat.method == "linear model") {
+      # Formatting specific to the linear model output (no mixed effect)
+      
+      # Correct pval and adjpval column name
+      colnames(lm.out)[2] <- "pval"
+      colnames(lm.out)[3] <- "FDR"
+      
+      # Correct pval column name and convert to dataframe 
+      test.results <- data.frame(lm.out)
+      
+      # Create a column for contrast
+      test.results$Contrast <- paste0(groups[1],"_",groups[2])
+      
+      # Create a column for gene and remove gene rownames
+      test.results$Gene <- row.names(test.results)
+      rownames(test.results) <- NULL
+      
+      # Create a column for the group
+      test.results$Subset <- region
+      
+      # Reorganize columns of final results
+      test.results <-
+        test.results[, c("Gene",
+                         "Subset",
+                         "Contrast",
+                         "Estimate",
+                         "pval",
+                         "FDR")]
+    } else {
+      stop("No valid statistics method was available for the data")
     }
+    
+    # Add results of current contrast to overall results
+    results <- rbind(results, test.results)
   }
   
   #Change the names of columns for table:
@@ -282,7 +496,7 @@ diffExpr <- function(object,
   pval.colname <- paste0(conname, "_pval")
   fdr.colname <- paste0(conname, "_adjpval")
   colnames(results) <-
-    sub("Pr\\(>\\|t\\|\\)", pval.colname, colnames(results))
+    sub("pval", pval.colname, colnames(results))
   colnames(results) <-
     sub("Estimate", logFC.colname, colnames(results))
   colnames(results) <- sub("FDR", fdr.colname, colnames(results))
